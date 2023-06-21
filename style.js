382af8e1d1e5b0ef20c921b5e416fcef95e7aabf
Balaji <vinothkumarbalaji150@gmail.com>
2023-06-21 20:35:03 +0530

    Delete style.js

--- style.js
+++
@@ -1,6 +0,0 @@
-let copy = (textId) => {
-    // select the text input field element
-    document.getElementById(textId).select();
-    // select the copied text to clipboard 
-    document.execCommand("copy");
-};
